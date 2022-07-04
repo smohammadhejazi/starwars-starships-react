723663e02c2b8ba24ddaa62acb871c13dd19ee87
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-07-04 18:21:14 +0430

    Write conditional rendering of components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import MovieList from "./components/MovieList.jsx";
+import Starships from "./components/StarShips";
+import "./App.css";
+
+
+const FILMS_API = "https://swapi.dev/api/films/"
+
 
 function App() {
+  const [showMovies, setShowMovies] = useState(true)
+  const [starshipIdx, setStarshipIdx] = useState(0)
+  const [loading, setloading] = useState(true);
+  const [moviesList, setMoviesList] = useState([]);
+
+  useEffect(() => {
+    fetch(FILMS_API)
+    .then(response => response.json())
+    .then(filmsJson => {
+      setMoviesList(filmsJson["results"]);
+      setloading(false);
+    });
+    // setMoviesList(MOVIES_LIST);
+    // setloading(false);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content-box" id="content-box">
+      {
+        loading ? (
+          <p>Loading...</p>
+        ):
+        (
+          <>
+          {
+            showMovies ? (
+              <MovieList setShowMovies={setShowMovies} setStarshipIdx={setStarshipIdx} moviesList={moviesList}></MovieList>
+            ):
+            (
+              <Starships setShowMovies={setShowMovies} movie={moviesList[starshipIdx]} ></Starships>
+            )
+          }
+          </>
+        )
+      }
+
     </div>
   );
 }
